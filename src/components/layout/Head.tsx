5b58001fea5471ddb4fe0e86e68746ad1ff1df50
waiwong-nc <WAIWONG.NC@gmail.com>
2023-04-02 10:22:51 +0100

    v1.0.0 deploy

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -5,9 +5,21 @@ const Head = () => {
 
   const [displayProfile, setDisplayProfile ] = useState<boolean>(false)
 
-   function displayProfileController(){
-      setDisplayProfile((prevState:boolean) => !prevState);
+   function displayProfileController():void{
+      setDisplayProfile((prevState:boolean) => {
+
+        // fixBodyPosition(!prevState);
+        return  !prevState;
+      });
    }
+
+  //  function fixBodyPosition(fix:boolean):void{
+  //   if (fix) {
+  //     document.body.style.position = "fixed"
+  //   } else {
+  //       document.body.style.position = "unset";
+  //   }
+  //  }
 
     return (
       <div className="head">
